ab07cc78d65db038682b482284b5823eb708e36e
pedro fico <adam.destensky@seznam.cz>
2022-07-30 22:18:17 +0200

    main 1 verze

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
 
+// testovaci projekt, na kterem se ucim pracovat s platform IO a git hubem
+// rozblika vestavenou diodu na Arduino Nano 168
+// a soucasne aktivuje seriovy monitor a bude vypisovat stav LED
+
+#define  LED  13            // vestavena LED
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(LED,OUTPUT);
+  digitalWrite(LED,LOW);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(800);              // blikani LED stale dokola
+  digitalWrite(LED,HIGH);
+  Serial.println("stav LED: ON");
+  delay(800);
+  digitalWrite(LED,LOW);
+  Serial.println("stav LED: OFF");
 }
